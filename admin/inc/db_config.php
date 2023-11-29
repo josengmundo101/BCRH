83f55c3b96d0067b83b4b685b8c8b1b623b4e1b5
JLuciloQuinte <josengmundosince2001@gmail.com>
2023-11-30 00:58:56 +0800

    adding admin panel - dashboard, rooms, settings and adding db_config and ajax

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -11,4 +11,53 @@
         die("Connection Failed:".mysqli_connect_error());
     }
 
+    function filteration($data){
+        foreach($data as $key => $values){
+            $data[$key] = trim($values);
+            $data[$key] = stripcslashes($values);
+            $data[$key] = htmlspecialchars($values);
+            $data[$key] = strip_tags($values);
+        }
+        return $data;
+    }
+    
+    function select($sql,$values,$datatypes)
+    {
+        $con = $GLOBALS['con'];
+        if($stmt = mysqli_prepare($con,$sql)){
+            mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+            if(mysqli_stmt_execute($stmt)){
+                $res = mysqli_stmt_get_result($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;
+            }
+            else{
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Select");
+            }
+        }
+        else{
+            die("Query cannot be executed - Select");
+        }
+    }
+
+    // function update($sql,$values,$datatypes)
+    // {
+    //     $con = $GLOBALS['con'];
+    //     if($stmt = mysqli_prepare($con,$sql)){
+    //         mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+    //         if(mysqli_stmt_execute($stmt)){
+    //             $res = mysqli_stmt_affected_rows($stmt);
+    //             mysqli_stmt_close($stmt);
+    //             return $res;
+    //         }
+    //         else{
+    //             mysqli_stmt_close($stmt);
+    //             die("Query cannot be executed - Update");
+    //         }
+    //     }
+    //     else{
+    //         die("Query cannot be executed - Update");
+    //     }
+    // }
 ?>
--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,13 @@
+<?php 
+    require('inc/essentials.php');
+    require('inc/db_config.php');
+
+    session_start();
+        if((isset($_SESSION['adminLogin']) && $_SESSION['adminLogin']==true)){
+            redirect('dashboard.php');
+        }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,37 +15,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE-edge">    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Login Panel</title>
-    <?php require('inc/links.php') ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="inc/style.css">
+    <?php require('inc/links.php'); ?>
 
-    <style>
-        div.login-form{
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 400px;
-        }
-    </style>
+
  </head>
 <body class="bg-light">
    
     <div class="login-form text-center rounded bg-white shadow overflow-hidden">
-        <form>
+        <form method="POST">
             <h4 class="bg-dark text-white py-3">ADMIN LOGIN PANEL</h4>
             <div class="p-4">
                 <div class="mb-3">
-                    <input name="admin_name" type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
+                    <input name="admin_name" required type="text" class="form-control shadow-none text-center" placeholder="Admin Name">
                 </div>
                 <div class="mb-4">
-                    <input name="admin_pass" type="password" class="form-control shadow-none text-center" placeholder="Passowrd">
+                    <input name="admin_pass" required type="password" class="form-control shadow-none text-center" placeholder="Passowrd">
                 </div>
                 <button name="login" type="submit" class="btn text-white custom-bg shadow-none">LOGIN</button>
             </div>
         </form>
     </div>
 
+    <?php
 
+        if(isset($_POST['login']))
+        {
+            $frm_data = filteration($_POST);
+         
+            $query = "SELECT * FROM `admin_cred` WHERE `admin_name`=? AND `admin_pass`=? ";
+            $values = [$frm_data['admin_name'],$frm_data['admin_pass']];
+            
+            $res = select($query,$values,"ss");
+            if($res->num_rows==1){
+                $row = mysqli_fetch_assoc($res);
+         
+                $_SESSION['adminLogin'] = true;
+                $_SESSION['adminID'] = $row['sr_no'];
+                redirect('dashboard.php');
+            }else{
+                alert('error','Login failed - Invalid Credentials!');
+            }
+        }
+    ?>
+    
 
-    <?php require('inc/script.php') ?>
+    <?php require('inc/script.php'); ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
