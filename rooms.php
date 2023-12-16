1404c911d639e5c695b5d49fbebe8c4cb60aabe6
JLuciloQuinte <josengmundosince2001@gmail.com>
2023-12-17 01:41:37 +0800

    adding rooms in frontend

--- rooms.php
+++ rooms.php
@@ -18,7 +18,7 @@
         <div class="h-line bg-dark"></div>
     </div>
 
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
 
            <div class="col-lg-3 2col-md-12 mb-lg-0 mb-4">
@@ -71,63 +71,98 @@
            </div>
 
            <div class="col-lg-9 col-md-12 px-4">
-                <div class="card mb-4 border-0 shadow">
-                    <div class="row g-0 p-3 align-items-center">
-                        <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
-                        <img src="images/rooms/alroom1.1.jpg" class="img-fluid rounded" >
-                        </div>
-                        <div class="col-md-5 px-lg-3 px-md-3 px-0">
-                            <h5 class="mb-3">Almont's Room</h5>
-                            <div class="features mb-3">
-                                <h6 class="mb-1">Features</h6>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    2 Rooms
-                                </span>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                1 Bathroom 
-                                </span>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    1 Balcony
-                                </span>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    1 Sofa
-                                </span>
-                            </div>
-                            <div class="facilities mb-3">
-                                <h6 class="mb-1">Facilities</h6>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    Wifi
-                                </span>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    Television
-                                </span>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    AC
-                                </span>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    Room heater
-                                </span>
-                            </div>
-                            <div class="guests">
-                                <h6 class="mb-1">Guests</h6>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    5 Adults
-                                </span>
-                                <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
-                                    4 Children
-                                </span>
-                            </div>
-        
-                        </div>
-                        
 
-                        <div class="col-md-2 text-align-center">
-                            <h6 class="mb-4">₱200 per night</h6>
-                            <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
-                             <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
+            <?php 
+            
+                $room_res = select("SELECT * FROM `rooms` WHERE `status`=? AND `removed`=?",[1,0],'ii');
+
+                while($room_data = mysqli_fetch_assoc($room_res)){
+                    //get features of rooms
+
+                    $fea_q = mysqli_query($con,"SELECT f.name FROM `features` f 
+                            INNER JOIN `room_features` rfea ON f.id = rfea.features_id 
+                            WHERE rfea.room_id = '$room_data[id]'");
+
+                    $features_data = "";
+                    while($fea_row = mysqli_fetch_assoc($fea_q)){
+                        $features_data .="<span class='badge rounded-pill bg-light text-dark text-wrap'>
+                            $fea_row[name]                
+                        </span>";
+
+                        
+                    }
+
+                    //get features of rooms
+
+                    $fac_q = mysqli_query($con,"SELECT f.name FROM `facilities` f 
+                            INNER JOIN `room_facilities` rfac ON f.id = rfac.facilities_id 
+                            WHERE rfac.room_id = '$room_data[id]'");
+
+                    $facilities_data = "";    
+                    while($fac_row = mysqli_fetch_assoc($fac_q)){
+                        $facilities_data .="<span class='badge rounded-pill bg-light text-dark text-wrap'>
+                            $fac_row[name]                
+                        </span>";
+                    }
+
+                    //get thumbnail of image
+
+                    $room_thumb = ROOMS_IMG_PATH."simple1.jpg";
+                    $thumb_q = mysqli_query($con, "SELECT * FROM `room_images` 
+                                WHERE `room_id`='$room_data[id]'
+                                AND `thumb`='1'");
+
+                    if(mysqli_num_rows($thumb_q)>0){
+                        $thumb_res = mysqli_fetch_assoc($thumb_q);
+                        $room_thumb = ROOMS_IMG_PATH.$thumb_res['image'];
+                    }
+
+
+                    //print room card
+                    echo <<<data
+                        <div class="card mb-4 border-0 shadow">
+                            <div class="row g-0 p-3 align-items-center">
+                                <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+                                <img src="images/rooms/alroom1.1.jpg" class="img-fluid rounded" >
+                                </div>
+                                <div class="col-md-5 px-lg-3 px-md-3 px-0">
+                                    <h5 class="mb-3">$room_data[name]</h5>
+                                    <div class="features mb-3">
+                                        <h6 class="mb-1">Features</h6>
+                                        $features_data
+                                    </div>
+                                    <div class="facilities mb-3">
+                                        <h6 class="mb-1">Facilities</h6>
+                                        $facilities_data
+                                    </div>
+                                    <div class="guests">
+                                        <h6 class="mb-1">Guests</h6>
+                                        <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
+                                            $room_data[adult] Adults
+                                        </span>
+                                        <span class="badge rounded-pill bg-light text-dark text-wrap lh-base">
+                                            $room_data[children] Children
+                                        </span>
+                                    </div>
+                
+                                </div>
+                                
+
+                                <div class="col-md-2 text-align-center">
+                                    <h6 class="mb-4">₱200 per night</h6>
+                                    <a href="#" class="btn btn-sm w-100 text-white custom-bg shadow-none mb-2">Book Now</a>
+                                    <a href="#" class="btn btn-sm w-100 btn-outline-dark shadow-none">More details</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    data;
+                }   
+
+                    
+            
+            ?>
+                
+                
            </div>
            
 
